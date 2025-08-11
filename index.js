2cda416d8e8945b5882fe7fdc364896e4eb633f4
amandamirandak <amanda.miranda@ifsp.edu.br>
2025-08-11 17:49:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Importa a função criarItemDaLista do arquivo especificado
 import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
 // Importa a função verificarListaVazia do arquivo especificado
-import verificarListaVazia from "./scripts/verificarListaVazia.js";
+//import verificarListaVazia from "./scripts/verificarListaVazia.js";
 
 // Seleciona o input onde o usuário digita o item
 const inputItem = document.getElementById("input-item");
@@ -19,11 +19,11 @@ botaoAdicionar.addEventListener("click", (evento) => {
     // Adiciona o novo item à lista de compras
     listaDeCompras.appendChild(itemDaLista);
     // Verifica se a lista está vazia e atualiza a interface se necessário
-    verificarListaVazia(listaDeCompras);
+    //verificarListaVazia(listaDeCompras);
     // Limpa o campo de input após adicionar o item
     inputItem.value = "";
 });
 
 // Faz uma verificação inicial se a lista está vazia ao carregar a página
-verificarListaVazia(listaDeCompras);
+//verificarListaVazia(listaDeCompras);
 
